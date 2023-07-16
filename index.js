d1b901cb64eb89a19b87b0fe3fadfbb91e5532f0
Mateus Schverz <mate_fs@hotmail.com>
2023-07-16 12:14:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./server.js')
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port,() => {
     console.log('Example app listennig at http://localhost:'+port)
